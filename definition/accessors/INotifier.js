3a80e38c0346bce21678bb654e8e343def4633f1
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2020-10-20 21:47:57 +0530

    Add build files

--- definition/accessors/INotifier.js
+++ definition/accessors/INotifier.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var TypingScope;
+(function (TypingScope) {
+    TypingScope["Room"] = "room";
+})(TypingScope = exports.TypingScope || (exports.TypingScope = {}));
 
 //# sourceMappingURL=INotifier.js.map
