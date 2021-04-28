da23204e518eae04b35b711f036d59ea22eb53b9
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2021-04-28 19:39:54 +0530

    Remove logs

--- src/server/compiler/AppCompiler.ts
+++ src/server/compiler/AppCompiler.ts
@@ -33,10 +33,6 @@ export class AppCompiler {
         const logLevelSetting = storage.settings.log_level;
         const logLevel = logLevelSetting && (logLevelSetting.value || logLevelSetting.packageValue) || 0;
 
-        console.log('APP SETTINGS');
-        console.log(logLevelSetting);
-        console.log(logLevel);
-
         const customRequire = Utilities.buildCustomRequire(files, storage.info.id, logLevel);
         const context = vm.createContext({ require: customRequire, exports, process: {}, console });
 
